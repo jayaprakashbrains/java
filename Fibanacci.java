4593e5fa1287df092f3da63a9ec35a5aa90a8dda
jp <jayaprakashbrains@gmail.com>
2017-11-04 22:25:55 +0530

    hi

---
+++ Fibanacci.java
@@ -0,0 +1,19 @@
+import java.io.*;
+public class Fibanacci {
+public static void main(String ar[])
+{
+int a=0,b=1,c=0,i;
+
+for(i=0;i<10;i++)
+	
+{
+	c=a+b;
+	a=b;
+	b=c;
+	
+	System.out.println(c);
+	
+	}
+}
+}
+
